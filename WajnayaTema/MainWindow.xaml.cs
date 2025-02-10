794c67b6983252e88239e021cdcbee280a2eba40
KerlonDes <132078958+KerlonDes@users.noreply.github.com>
2025-02-10 08:47:51 +0500

    Переходы, ну немного функционала короч

--- WajnayaTema/MainWindow.xaml.cs
+++ WajnayaTema/MainWindow.xaml.cs
@@ -10,8 +10,10 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
+using System.Windows.Media.Media3D;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using WajnayaTema.Windows;
 
 namespace WajnayaTema
 {
@@ -20,9 +22,28 @@ namespace WajnayaTema
     /// </summary>
     public partial class MainWindow : Window
     {
+        BDEntities entities;
         public MainWindow()
         {
             InitializeComponent();
+            entities = new BDEntities();
+            LoadData();
+            //NameMat. = entities.Materials;
+        }
+        private void LoadData()
+        {
+            // Загружаем данные из базы
+            var materials = entities.Materials.ToList();
+
+            // Устанавливаем DataContext для DataGrid
+            DataContext = materials;
+        }
+        public BDEntities Entities;
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            WindowStaff windowStaff = new WindowStaff();
+            windowStaff.Show();
+            this.Close();
         }
     }
 }
---
+++ WajnayaTema/Windows/WindowStaff.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WajnayaTema.Windows
+{
+    /// <summary>
+    /// Логика взаимодействия для WindowStaff.xaml
+    /// </summary>
+    public partial class WindowStaff : Window
+    {
+        public WindowStaff()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            MainWindow mainWindow = new MainWindow();
+            mainWindow.Show();
+            this.Close();
+        }
+    }
+}
